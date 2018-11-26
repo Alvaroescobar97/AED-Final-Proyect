8e6fee3f16ae107bfcc645a86c6e176588f9a380
Sebastian Correa Villada <1005896113@academico.icesi.edu.co>
2018-11-26 15:57:09 -0500

    Player class partially documented

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,29 +1,49 @@
 package model;
 
+/**
+ * Player class, represents the main character of the game, Bomber.
+ * 
+ * @author Luis A. Rodriguez, Álvaro J. Escobar, Sebastián Correa.
+ * @version 1.0
+ * @since 2018-11-26
+ */
 public class Player {
 
 	private String nickname;
 	private Step steps;
-	
+
+	/**
+	 * This is the Player class constructor, it is used to create instances of this
+	 * class.
+	 * 
+	 * @param nickname
+	 *            the name of the character.
+	 */
 	public Player(String nickname) {
 		this.nickname = nickname;
 		this.steps = new Step(0);
 	}
-	
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+
+	/**
+	 * 
+	 * @return
+	 */
 	public Step getSteps() {
 		return steps;
 	}
+
+	/**
+	 * 
+	 * @param steps
+	 */
 	public void setSteps(Step steps) {
 		this.steps = steps;
 	}
-	
+
+	/**
+	 * 
+	 */
 	public void move() {
-		
+
 	}
 }
