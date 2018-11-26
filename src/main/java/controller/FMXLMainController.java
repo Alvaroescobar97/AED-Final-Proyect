02fb97c87d382b3aaf44b4227084c7be91f5b263
Alvaro Jose Escobar  Gonzalez <1144099657@academico.icesi.edu.co>
2018-11-26 14:01:37 -0500

    Corrections of bugs on FXMLMainController

--- src/main/java/controller/FMXLMainController.java
+++ src/main/java/controller/FMXLMainController.java
@@ -1,11 +1,18 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class FMXLMainController implements Initializable {
 
@@ -24,7 +31,31 @@ public class FMXLMainController implements Initializable {
 	}
 
 	@FXML
-	public void startGame() {
+	public void eventExit(ActionEvent event) {
+
+	}
+
+	@FXML
+	public void startGame(ActionEvent event) {
+
+		FXMLLoader loader = new FXMLLoader();
+
+		AnchorPane gameViewParent;
+		try {
+			gameViewParent = (AnchorPane) loader.load(getClass().getResource("/designs/Stats.fxml").openStream());
+			FXMLGameController gameController = (FXMLGameController) loader.getController();
+
+			Scene gameScene = new Scene(gameViewParent);
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			window.setScene(gameScene);
+			window.alwaysOnTopProperty();
+			window.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@FXML
