997f9b2318ce5db4a4de18124d7b8a245633feca
LuisAlfredo <luisalfredo9905@gmail.com>
2018-11-26 15:55:05 -0500

    Finished the main GUI and the instructions GUI

--- src/main/java/controller/FXMLInstructionsController.java
+++ src/main/java/controller/FXMLInstructionsController.java
@@ -1,19 +1,29 @@
 package controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class FXMLInstructionsController {
+public class FXMLInstructionsController implements Initializable{
+	
 	@FXML
-	private Button btnBack;
+    private ImageView imgKey, imgSpace, imgBackground;
+
+    @FXML
+    private Button btnBack;
 
 	@FXML
 	void backMenu(ActionEvent event) {
@@ -32,5 +42,11 @@ public class FXMLInstructionsController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources){
+		
+        
 	}
 }
