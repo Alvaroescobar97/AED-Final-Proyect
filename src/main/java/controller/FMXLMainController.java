e989d0bb1e5a1c72371c5ae82d3cd924813e0f75
Jhusseth Vaironel Sanchez Arias <1061814203@academico.icesi.edu.co>
2018-11-26 09:25:11 -0500

    Class FXMLMainController

---
+++ src/main/java/controller/FMXLMainController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class FMXLMainController implements Initializable {
+
+	@FXML
+	private Button btnStartGame;
+
+	@FXML
+	private Button btnInstructions;
+
+	@FXML
+	private Button btnExit;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
+
+	@FXML
+	public void startGame() {
+	}
+
+	@FXML
+	public void showInstructions() {
+	}
+
+	@FXML
+	public void exit() {
+	}
+
+}
