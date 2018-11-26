ab6a80a9680cf96e213a4b9c1a4a7957843ebd97
Sebastian Correa Villada <1005896113@academico.icesi.edu.co>
2018-11-26 16:31:32 -0500

    VertexWeightComparator class fully documented

--- src/main/java/graphEstructure/VertexWeightComparator.java
+++ src/main/java/graphEstructure/VertexWeightComparator.java
@@ -2,10 +2,19 @@ package graphEstructure;
 
 import java.util.Comparator;
 
+/**
+ * VertexWeightComparator class, used only to compare a pair of vertexes.
+ * 
+ * @author Luis A. Rodriguez, Álvaro J. Escobar, Sebastián Correa.
+ * @version 1.0
+ * @since 2018-11-26
+ * @param <T>
+ *            vertex representation, bases on the kind of problem to solve.
+ */
 public class VertexWeightComparator<T extends Comparable<T>> implements Comparator<T> {
 	@Override
 	public int compare(T o1, T o2) {
 		return o1.compareTo(o2);
 	}
-	
+
 }
